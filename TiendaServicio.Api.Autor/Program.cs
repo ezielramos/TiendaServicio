406b882098d645779bc4a1412e87b886b903d360
ezielramos <47543896+ezielramos@users.noreply.github.com>
2023-05-03 13:28:20 -0400

    docker containers

--- TiendaServicio.Api.Autor/Program.cs
+++ TiendaServicio.Api.Autor/Program.cs
@@ -1,5 +1,6 @@
-using FluentValidation.AspNetCore;
 using MediatR;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TiendaServicio.Api.Autor.Aplicacion;
 using TiendaServicio.Api.Autor.Persistencia;
@@ -11,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
 
-builder.Services.AddDbContext<ContextoAutor>(options => {
+builder.Services.AddDbContext<ContextoAutor>(options =>
+{
 	options.UseNpgsql(builder.Configuration.GetConnectionString("ConexionDatabase"));
 });
 
@@ -21,8 +23,30 @@ builder.Services.AddAutoMapper(typeof(Consulta.Manejador));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+
+
+
+
+
+////Apply migrations at runtime
+//using (var scope = app.Services.CreateScope())
+//{
+//	var db = scope.ServiceProvider.GetRequiredService<ContextoAutor>();
+//	db.Database.Migrate();
+//}
+
+////CHECK DATABASE CONECTION:
+//app.MapGet("/dbconexion", async ([FromServices] ContextoAutor dbContext)  => 
+//{
+//	dbContext.Database.EnsureCreated();//si la base de datos no esta creada la crea
+//	var sql = dbContext.Database.GenerateCreateScript();
+//	return Results.Ok(sql);
+//});
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
